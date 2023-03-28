574e57abec1ed5be75c80116b9f024d9f025c300
unknown <rafael.zottesso@ifpr.edu.br>
2023-03-28 16:24:49 -0300

    Criação das views de List, Delete e Detail. Criação das listas para campus, curso e estagio

--- estagif/cadastros/urls.py
+++ estagif/cadastros/urls.py
@@ -1,14 +1,77 @@
 from django.urls import path
-from .views import CampusCreate, CampusUpdate
-from .views import CursoCreate, CursoUpdate
+from .views import CampusCreate, CampusUpdate, CampusList, CampusDetail, CampusDelete
+from .views import CursoCreate, CursoUpdate, CursoList, CursoDetail, CursoDelete
+from .views import EstudanteCreate, EstudanteUpdate, EstudanteList, EstudanteDetail, EstudanteDelete
+from .views import ServidorCreate, ServidorUpdate, ServidorList, ServidorDetail, ServidorDelete
+from .views import UnidadeConcedenteCreate, UnidadeConcedenteUpdate, UnidadeConcedenteList, UnidadeConcedenteDetail, UnidadeConcedenteDelete
+from .views import ResponsavelCreate, ResponsavelUpdate, ResponsavelList, ResponsavelDetail, ResponsavelDelete
+from .views import IntermediarioCreate, IntermediarioUpdate, IntermediarioList, IntermediarioDetail, IntermediarioDelete
+from .views import SituacaoCreate, SituacaoUpdate, SituacaoList, SituacaoDetail, SituacaoDelete
+from .views import EstagioCreate, EstagioUpdate, EstagioList, EstagioDetail, EstagioDelete
+from .views import RelatorioCreate, RelatorioUpdate, RelatorioList, RelatorioDetail, RelatorioDelete
 
 urlpatterns = [
     
     path("cadastrar/campus/", CampusCreate.as_view(), name="cadastrar-campus"),
     path("cadastrar/curso/", CursoCreate.as_view(), name="cadastrar-curso"),
-
+    path("cadastrar/estudante/", EstudanteCreate.as_view(), name="cadastrar-estudante"),
+    path("cadastrar/servidor/", ServidorCreate.as_view(), name="cadastrar-servidor"),
+    path("cadastrar/unidade-concedente/", UnidadeConcedenteCreate.as_view(), name="cadastrar-unidade-concedente"),
+    path("cadastrar/responsavel-uc/", ResponsavelCreate.as_view(), name="cadastrar-responsavel"),
+    path("cadastrar/intermediario/", IntermediarioCreate.as_view(), name="cadastrar-intermediario"),
+    path("cadastrar/situacao-estagio/", SituacaoCreate.as_view(), name="cadastrar-situacao"),
+    path("cadastrar/estagio/", EstagioCreate.as_view(), name="cadastrar-estagio"),
+    path("cadastrar/relatorio-estagio/", RelatorioCreate.as_view(), name="cadastrar-relatorio"),
+    
     path("editar/campus/<int:pk>/", CampusUpdate.as_view(), name="editar-campus"),
     path("editar/curso/<int:pk>/", CursoUpdate.as_view(), name="editar-curso"),
+    path("editar/estudante/<int:pk>/", EstudanteUpdate.as_view(), name="editar-estudante"),
+    path("editar/servidor/<int:pk>/", ServidorUpdate.as_view(), name="editar-servidor"),
+    path("editar/unidade-concedente/<int:pk>/", UnidadeConcedenteUpdate.as_view(), name="editar-unidade-concedente"),
+    path("editar/responsavel-uc/<int:pk>/", ResponsavelUpdate.as_view(), name="editar-responsavel"),
+    path("editar/intermediario/<int:pk>/", IntermediarioUpdate.as_view(), name="editar-intermediario"),
+    path("editar/situacao-estagio/<int:pk>/", SituacaoUpdate.as_view(), name="editar-situacao"),
+    path("editar/estagio/<int:pk>/", EstagioUpdate.as_view(), name="editar-estagio"),
+    path("editar/relatorio-estagio/<int:pk>/", RelatorioUpdate.as_view(), name="editar-relatorio"),
+
+    path("excluir/campus/<int:pk>/", CampusDelete.as_view(), name="excluir-campus"),
+    path("excluir/curso/<int:pk>/", CursoDelete.as_view(), name="excluir-curso"),
+    path("excluir/estudante/<int:pk>/", EstudanteDelete.as_view(), name="excluir-estudante"),
+    path("excluir/servidor/<int:pk>/", ServidorDelete.as_view(), name="excluir-servidor"),
+    path("excluir/unidade-concedente/<int:pk>/", UnidadeConcedenteDelete.as_view(), name="excluir-unidade-concedente"),
+    path("excluir/responsavel-uc/<int:pk>/", ResponsavelDelete.as_view(), name="excluir-responsavel"),
+    path("excluir/intermediario/<int:pk>/", IntermediarioDelete.as_view(), name="excluir-intermediario"),
+    path("excluir/situacao-estagio/<int:pk>/", SituacaoDelete.as_view(), name="excluir-situacao"),
+    path("excluir/estagio/<int:pk>/", EstagioDelete.as_view(), name="excluir-estagio"),
+    path("excluir/relatorio-estagio/<int:pk>/", RelatorioDelete.as_view(), name="excluir-relatorio"),
+
+    path("listar/campus/", CampusList.as_view(), name="listar-campus"),
+    path("listar/curso/", CursoList.as_view(), name="listar-curso"),
+    path("listar/estudante/", EstudanteList.as_view(), name="listar-estudante"),
+    path("listar/servidor/", ServidorList.as_view(), name="listar-servidor"),
+    path("listar/unidade-concedente/", UnidadeConcedenteList.as_view(), name="listar-unidade-concedente"),
+    path("listar/responsavel-uc/", ResponsavelList.as_view(), name="listar-responsavel"),
+    path("listar/intermediario/", IntermediarioList.as_view(), name="listar-intermediario"),
+    path("listar/situacao-estagio/", SituacaoList.as_view(), name="listar-situacao"),
+    path("listar/estagio/", EstagioList.as_view(), name="listar-estagio"),
+    path("listar/relatorio-estagio/", RelatorioList.as_view(), name="listar-relatorio"),
+
+    path("detalhar/campus/<int:pk>/", CampusDetail.as_view(), name="detalhar-campus"),
+    path("detalhar/curso/<int:pk>/", CursoDetail.as_view(), name="detalhar-curso"),
+    path("detalhar/estudante/<int:pk>/", EstudanteDetail.as_view(), name="detalhar-estudante"),
+    path("detalhar/servidor/<int:pk>/", ServidorDetail.as_view(), name="detalhar-servidor"),
+    path("detalhar/unidade-concedente/<int:pk>/", UnidadeConcedenteDetail.as_view(), name="detalhar-unidade-concedente"),
+    path("detalhar/responsavel-uc/<int:pk>/", ResponsavelDetail.as_view(), name="detalhar-responsavel"),
+    path("detalhar/intermediario/<int:pk>/", IntermediarioDetail.as_view(), name="detalhar-intermediario"),
+    path("detalhar/situacao-estagio/<int:pk>/", SituacaoDetail.as_view(), name="detalhar-situacao"),
+    path("detalhar/estagio/<int:pk>/", EstagioDetail.as_view(), name="detalhar-estagio"),
+    path("detalhar/relatorio-estagio/<int:pk>/", RelatorioDetail.as_view(), name="detalhar-relatorio"),
+
+
+
+
+    # path("editar/campus/<int:pk>/", CampusUpdate.as_view(), name="editar-campus"),
+    # path("editar/curso/<int:pk>/", CursoUpdate.as_view(), name="editar-curso"),
 
 
 ]
--- estagif/cadastros/views.py
+++ estagif/cadastros/views.py
@@ -1,8 +1,10 @@
 from .models import Campus, Curso, UnidadeConcedente, Responsavel
-
+from .models import Estudante, Servidor, Intermediario, Situacao, Estagio, Relatorio
 from django.urls import reverse_lazy
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 
@@ -11,14 +13,83 @@ class CampusCreate(CreateView):
     model = Campus
     fields = ["nome"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("pagina-inicial")
+    success_url = reverse_lazy("listar-campus")
 
 
 class CursoCreate(CreateView):
     model = Curso
     fields = ["nome"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("pagina-inicial")
+    success_url = reverse_lazy("listar-curso")
+
+
+class EstudanteCreate(CreateView):
+    model = Estudante
+    fields = [
+        "nome", "cpf", "data_nascimento", "telefone", "email",
+        "matricula", "curso", "campus",
+    ]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-estudante")
+
+
+class ServidorCreate(CreateView):
+    model = Servidor
+    fields = [
+        "nome","cpf","data_nascimento","telefone",
+        "email","siape","campus",
+    ]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-servidor")
+
+
+class UnidadeConcedenteCreate(CreateView):
+    model = UnidadeConcedente
+    fields = ["nome", "documento", "telefone", "email",]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-unidade-concedente")
+
+
+class ResponsavelCreate(CreateView):
+    model = Responsavel
+    fields = ["nome", "email", "telefone", "cpf", "empresa",]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-responsavel")
+
+
+class IntermediarioCreate(CreateView):
+    model = Intermediario
+    fields = ["nome"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-intermediario")
+
+
+class SituacaoCreate(CreateView):
+    model = Situacao
+    fields = ["nome"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-situacao")
+
+
+class EstagioCreate(CreateView):
+    model = Estagio
+    fields = [
+        "estudante", "intermediario", "unidade_concedente", "responssvel_empresa",
+        "orientador", "data_inicio", "data_termino", "ch_semanal",
+        "situacao", "observacoes",
+    ]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-estagio")
+
+
+class RelatorioCreate(CreateView):
+    model = Relatorio
+    fields = [
+        "estagio", "data_inicio", "data_termino",
+        "tipo", "entregue", "data_cadastro",
+    ]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-relatorio")
 
 
 ##################################################
@@ -28,11 +99,252 @@ class CampusUpdate(UpdateView):
     model = Campus
     fields = ["nome"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("pagina-inicial")
+    success_url = reverse_lazy("listar-campus")
 
 
 class CursoUpdate(UpdateView):
     model = Curso
     fields = ["nome"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("pagina-inicial")
+    success_url = reverse_lazy("listar-curso")
+
+
+class EstudanteUpdate(UpdateView):
+    model = Estudante
+    fields = [
+        "nome", "cpf", "data_nascimento", "telefone", "email",
+        "matricula", "curso", "campus",
+    ]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-estudante")
+
+
+class ServidorUpdate(UpdateView):
+    model = Servidor
+    fields = [
+        "nome","cpf","data_nascimento","telefone",
+        "email","siape","campus",
+    ]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-servidor")
+
+
+class UnidadeConcedenteUpdate(UpdateView):
+    model = UnidadeConcedente
+    fields = ["nome", "documento", "telefone", "email",]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-unidade-concedente")
+
+
+class ResponsavelUpdate(UpdateView):
+    model = Responsavel
+    fields = ["nome", "email", "telefone", "cpf", "empresa",]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-responsavel")
+
+
+class IntermediarioUpdate(UpdateView):
+    model = Intermediario
+    fields = ["nome"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-intermediario")
+
+
+class SituacaoUpdate(UpdateView):
+    model = Situacao
+    fields = ["nome"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-situacao")
+
+
+class EstagioUpdate(UpdateView):
+    model = Estagio
+    fields = [
+        "estudante", "intermediario", "unidade_concedente", "responssvel_empresa",
+        "orientador", "data_inicio", "data_termino", "ch_semanal",
+        "situacao", "observacoes",
+    ]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-estagio")
+
+
+class RelatorioUpdate(UpdateView):
+    model = Relatorio
+    fields = [
+        "estagio", "data_inicio", "data_termino",
+        "tipo", "entregue", "data_cadastro",
+    ]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("listar-relatorio")
+
+
+##################################################
+
+class CampusDelete(DeleteView):
+    model = Campus
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-campus")
+
+
+class CursoDelete(DeleteView):  
+    model = Curso
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-curso")
+
+
+class EstudanteDelete(DeleteView):
+    model = Estudante
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-estudante")
+
+
+class ServidorDelete(DeleteView):
+    model = Servidor
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-servidor")
+
+
+class UnidadeConcedenteDelete(DeleteView):
+    model = UnidadeConcedente
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-unidade-concedente")
+
+
+class ResponsavelDelete(DeleteView):
+    model = Responsavel
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-responsavel")
+
+
+class IntermediarioDelete(DeleteView):
+    model = Intermediario
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-intermediario")
+
+
+class SituacaoDelete(DeleteView):
+    model = Situacao
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-situacao")
+
+
+class EstagioDelete(DeleteView):
+    model = Estagio
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-estagio")
+
+
+class RelatorioDelete(DeleteView):
+    model = Relatorio
+    template_name = "cadastros/delete.html"
+    success_url = reverse_lazy("listar-relatorio")
+
+
+##################################################
+
+
+class CampusList(ListView):
+    model = Campus
+    template_name = "cadastros/list/campus.html"
+
+
+class CursoList(ListView):
+    model = Curso
+    template_name = "cadastros/list/curso.html"
+
+
+class EstudanteList(ListView):  
+    model = Estudante
+    template_name = "cadastros/list/estudante.html"
+
+
+class ServidorList(ListView):
+    model = Servidor
+    template_name = "cadastros/list/servidor.html"
+
+
+class UnidadeConcedenteList(ListView):
+    model = UnidadeConcedente
+    template_name = "cadastros/list/unidade-concedente.html"
+
+
+class ResponsavelList(ListView):
+    model = Responsavel
+    template_name = "cadastros/list/responsavel.html"
+
+
+class IntermediarioList(ListView):
+    model = Intermediario
+    template_name = "cadastros/list/intermediario.html"
+
+
+class SituacaoList(ListView):
+    model = Situacao
+    template_name = "cadastros/list/situacao.html"
+
+
+class EstagioList(ListView):
+    model = Estagio
+    template_name = "cadastros/list/estagio.html"
+
+
+class RelatorioList(ListView):
+    model = Relatorio
+    template_name = "cadastros/list/relatorio.html"
+
+
+##################################################
+
+
+class CampusDetail(DetailView):
+    model = Campus
+    template_name = "cadastros/detail/campus.html"
+
+
+class CursoDetail(DetailView):
+    model = Curso
+    template_name = "cadastros/detail/curso.html"
+
+
+class EstudanteDetail(DetailView):
+    model = Estudante
+    template_name = "cadastros/detail/estudante.html"
+
+
+class ServidorDetail(DetailView):
+    model = Servidor
+    template_name = "cadastros/detail/servidor.html"
+
+
+class UnidadeConcedenteDetail(DetailView):
+    model = UnidadeConcedente
+    template_name = "cadastros/detail/unidade-concedente.html"
+
+
+class ResponsavelDetail(DetailView):
+    model = Responsavel
+    template_name = "cadastros/detail/responsavel.html"
+
+
+class IntermediarioDetail(DetailView):
+    model = Intermediario
+    template_name = "cadastros/detail/intermediario.html"
+
+
+class SituacaoDetail(DetailView):
+    model = Situacao
+    template_name = "cadastros/detail/situacao.html"
+
+
+class EstagioDetail(DetailView):
+    model = Estagio
+    template_name = "cadastros/detail/estagio.html"
+
+
+class RelatorioDetail(DetailView):
+    model = Relatorio
+    template_name = "cadastros/detail/relatorio.html"
+
+
+##################################################
+
