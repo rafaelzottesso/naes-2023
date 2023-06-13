12fd21d9021aefd64222a9cf3ef22508f99f44a3
joaomarcosp-souza <rafael.zottesso@ifpr.edu.br>
2023-06-12 22:31:27 -0300

    Exemplo de queries, personalização de consultas e templates. Enviando listas e apresentando elas nos templates.

--- estagif/paginas/views.py
+++ estagif/paginas/views.py
@@ -1,5 +1,24 @@
+from cadastros.models import Estagio, Estudante, Servidor
 from django.views.generic import TemplateView
+import datetime
 
 
 class IndexView(TemplateView):
     template_name = "paginas/index.html"
+    
+    def get_context_data(self, *args, **kwargs):
+        dados = super().get_context_data(*args, **kwargs)
+
+        if (self.request.user.is_authenticated):
+
+            dados["estagios_estudante"] = Estagio.objects.filter(
+                estudante__usuario = self.request.user,
+                data_termino__lt=datetime.date.today()
+            )
+
+            dados["estagios_orientador"] = Estagio.objects.filter(
+                orientador__usuario=self.request.user,
+                data_termino__lt=datetime.date.today()
+            )
+
+        return dados
