708a33344ce884822931250401910bbba550701a
joaomarcosp-souza <joaomarcosp.sza@gmail.com>
2023-07-31 20:28:59 -0300

    Trabalhando com sessão

--- estagif/paginas/urls.py
+++ estagif/paginas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, CriarSessaoView, DeletarSessaoView
 
 urlpatterns = [
     
     path("", IndexView.as_view(), name="pagina-inicial"),
+    path("ativar/fazenda/<int:pk>/", CriarSessaoView.as_view(), name="criar-sessao"),
+    path("desativar/fazenda/", DeletarSessaoView.as_view(), name="deletar-sessao"),
 
 ]
--- estagif/paginas/views.py
+++ estagif/paginas/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from cadastros.models import Estagio, Estudante, Servidor
 from django.views.generic import TemplateView
 import datetime
@@ -39,3 +40,34 @@ class IndexView(TemplateView):
             ).select_related("estudante", "orientador", "unidade_concedente")
 
         return dados
+
+
+class CriarSessaoView(TemplateView):
+    template_name = "paginas/sessao.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        #fazenda = get_object_or_404(Fazenda, usuario=self.request.user, pk=self.kwargs["pk"])
+        # context["fazenda"] = fazenda
+        
+        # Armazena algo numa sessão (não pode ser objeto)
+        self.request.session["fazenda_pk"] = self.kwargs["pk"]
+
+        context["titulo"] = "Ativando fazenda..."
+        return context
+    
+
+class DeletarSessaoView(TemplateView):
+    template_name = "paginas/sessao.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        try:
+            # Retira um item da sessão
+            self.request.session.pop("fazenda_pk")
+        except:
+            pass
+
+        context["titulo"] = "Desativando fazenda..."
+
+        return context
